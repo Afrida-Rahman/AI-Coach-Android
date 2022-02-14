e43f931c3c7aea5a1254e9c3b42ff263a55ef067
afrida <arahman@mymedicalhub.com>
2022-02-14 21:55:57 +0600

    (DEV-7136)-Added intermediate code

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/ExerciseGuidelineFragment.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/ExerciseGuidelineFragment.kt
@@ -9,6 +9,14 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import org.tensorflow.lite.examples.poseestimation.core.ExerciseGuidelineImageListAdapter
 import org.tensorflow.lite.examples.poseestimation.exercise.home.HomeExercise
 
@@ -21,7 +29,7 @@ class ExerciseGuidelineFragment(
     private val patientId: String,
     private val tenant: String
 ) : Fragment() {
-
+    private lateinit var mediaSource: MediaSource
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,33 +37,45 @@ class ExerciseGuidelineFragment(
         val view = inflater.inflate(R.layout.fragment_exercise_guideline, container, false)
         val exerciseNameView: TextView = view.findViewById(R.id.exercise_name_guideline)
         val backButton: ImageButton = view.findViewById(R.id.back_button)
-        val videoView: VideoView = view.findViewById(R.id.video_view)
-        val playVideo: ImageView = view.findViewById(R.id.play_video)
-
+//        val videoView: VideoView = view.findViewById(R.id.video_view)
+//        val playVideo: ImageView = view.findViewById(R.id.play_video)
+        val playVideo: PlayerView = view.findViewById(R.id.video_view)
         val exercise = exerciseList[position]
         var instruction = exercise.instruction
         val imageUrls = exercise.imageUrls
         val videoUrls = exercise.videoUrls
 
-        val mediaController = MediaController(view.context)
-        playVideo.setOnClickListener {
-            mediaController.setAnchorView(videoView)
-            val pd = ProgressDialog(view.context)
-            pd.setMessage("Loading...")
-            pd.show()
+//        val mediaController = MediaController(view.context)
+//        playVideo.setOnClickListener {
+//            mediaController.setAnchorView(videoView)
+//            val pd = ProgressDialog(view.context)
+//            pd.setMessage("Loading...")
+//            pd.show()
+//
+//            val uri: Uri = Uri.parse(videoUrls)
+//            videoView.setMediaController(mediaController)
+//            videoView.setVideoURI(uri)
+//            videoView.requestFocus()
+//
+//            videoView.setOnPreparedListener {//close the progress dialog when buffering is done
+//                videoView.start()
+//                pd.dismiss()
+//                playVideo.visibility = View.GONE
+//            }
+//            playVideo.visibility = View.VISIBLE
+//        }
+        val exoplayer: SimpleExoPlayer = SimpleExoPlayer.Builder(view.context).build()
 
-            val uri: Uri = Uri.parse(videoUrls)
-            videoView.setMediaController(mediaController)
-            videoView.setVideoURI(uri)
-            videoView.requestFocus()
 
-            videoView.setOnPreparedListener {//close the progress dialog when buffering is done
-                videoView.start()
-                pd.dismiss()
-                playVideo.visibility = View.GONE
-            }
-            playVideo.visibility = View.VISIBLE
-        }
+        playVideo.player = exoplayer
+        exoplayer.setMediaSource(mediaSource)
+        exoplayer.prepare()
+        //media source
+        exoplayer.seekTo(0)
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(view.context, Util.getUserAgent(view.context, ""))
+        mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(Uri.parse(videoUrls)))
+        exoplayer.playWhenReady = true
+        exoplayer.play()
 
         backButton.setOnClickListener {
             parentFragmentManager.beginTransaction().apply {
@@ -81,5 +101,10 @@ class ExerciseGuidelineFragment(
         }
         adapter.adapter = ExerciseGuidelineImageListAdapter(view.context, imageUrls)
         return view
+    }
+
+    private fun initVideoView() {
+
+
     }
 }
