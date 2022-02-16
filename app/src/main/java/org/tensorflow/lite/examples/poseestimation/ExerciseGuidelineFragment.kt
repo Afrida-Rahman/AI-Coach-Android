73102e1fa26423dfd389ce2f81ef67319616aa93
afrida <arahman@mymedicalhub.com>
2022-02-16 18:52:46 +0600

    (DEV-7105-7106-7107-7199-7201-7203-7205-7207-7209-7211-7213-7215)-Add exercises

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/ExerciseGuidelineFragment.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/ExerciseGuidelineFragment.kt
@@ -1,6 +1,5 @@
 package org.tensorflow.lite.examples.poseestimation
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +11,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
-import com.google.android.exoplayer2.util.Util
 import org.tensorflow.lite.examples.poseestimation.core.ExerciseGuidelineImageListAdapter
 import org.tensorflow.lite.examples.poseestimation.exercise.home.HomeExercise
 
