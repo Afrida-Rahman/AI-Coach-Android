b41f0b4cc506e8eb27cb5cef1444b6afc78acdb5
afrida <arahman@mymedicalhub.com>
2022-03-14 21:07:07 +0600

    (DEV-7600)- Add start workout button on guideline page

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/ExerciseGuidelineFragment.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/ExerciseGuidelineFragment.kt
@@ -1,14 +1,19 @@
 package org.tensorflow.lite.examples.poseestimation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.MediaSource
@@ -17,6 +22,7 @@ import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import org.tensorflow.lite.examples.poseestimation.core.ExerciseGuidelineImageListAdapter
+import org.tensorflow.lite.examples.poseestimation.core.ExerciseInfoAdapter
 import org.tensorflow.lite.examples.poseestimation.exercise.home.HomeExercise
 
 
@@ -30,6 +36,8 @@ class ExerciseGuidelineFragment(
 ) : Fragment() {
     private lateinit var mediaSource: MediaSource
     private lateinit var exoplayer: ExoPlayer
+    private lateinit var viewGroup1: ViewGroup
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,6 +46,7 @@ class ExerciseGuidelineFragment(
         val exerciseNameView: TextView = view.findViewById(R.id.exercise_name_guideline)
         val backButton: ImageButton = view.findViewById(R.id.back_button)
         val playVideo: PlayerView = view.findViewById(R.id.video_view)
+        val startWorkoutButton: Button = view.findViewById(R.id.btn_start_workout_guideline)
 
         val exercise = exerciseList[position]
         var instruction = exercise.instruction
@@ -61,6 +70,28 @@ class ExerciseGuidelineFragment(
                 commit()
             }
             exoplayer.pause()
+        }
+        startWorkoutButton.setOnClickListener {
+//            showExerciseInformation(it.context, exercise)
+            val dialogView = LayoutInflater
+                .from(context)
+                .inflate(R.layout.exercise_info_modal, container, false)
+            val alertDialog = AlertDialog.Builder(view.context).setView(dialogView)
+            val imageSlider: ViewPager2 = dialogView.findViewById(R.id.exercise_image_slide)
+            imageSlider.adapter = ExerciseInfoAdapter(exercise.imageUrls)
+            alertDialog.setPositiveButton("Let's Start") { _, _ ->
+                val intent = Intent(context, ExerciseActivity::class.java).apply {
+                    putExtra(ExerciseActivity.ExerciseId, exercise.id)
+                    putExtra(ExerciseActivity.TestId, testId)
+                    putExtra(ExerciseActivity.Name, exercise.name)
+                    putExtra(ExerciseActivity.RepetitionLimit, exercise.maxRepCount)
+                    putExtra(ExerciseActivity.SetLimit, exercise.maxSetCount)
+                    putExtra(ExerciseActivity.ProtocolId, exercise.protocolId)
+                }
+                view.context.startActivity(intent)
+            }
+            alertDialog.setNegativeButton("Cancel") { _, _ -> }
+            alertDialog.show()
         }
 
         exerciseNameView.text = exercise.name
@@ -87,6 +118,28 @@ class ExerciseGuidelineFragment(
     override fun onPause() {
         super.onPause()
         exoplayer.pause()
+    }
+
+    private fun showExerciseInformation(context: Context, exercise: HomeExercise) {
+        val dialogView = LayoutInflater
+            .from(context)
+            .inflate(R.layout.exercise_info_modal, viewGroup1, false)
+        val alertDialog = AlertDialog.Builder(context).setView(dialogView)
+        val imageSlider: ViewPager2 = dialogView.findViewById(R.id.exercise_image_slide)
+        imageSlider.adapter = ExerciseInfoAdapter(exercise.imageUrls)
+        alertDialog.setPositiveButton("Let's Start") { _, _ ->
+            val intent = Intent(context, ExerciseActivity::class.java).apply {
+                putExtra(ExerciseActivity.ExerciseId, exercise.id)
+                putExtra(ExerciseActivity.TestId, testId)
+                putExtra(ExerciseActivity.Name, exercise.name)
+                putExtra(ExerciseActivity.RepetitionLimit, exercise.maxRepCount)
+                putExtra(ExerciseActivity.SetLimit, exercise.maxSetCount)
+                putExtra(ExerciseActivity.ProtocolId, exercise.protocolId)
+            }
+            context.startActivity(intent)
+        }
+        alertDialog.setNegativeButton("Cancel") { _, _ -> }
+        alertDialog.show()
     }
 
     private fun buildMediaSource(videoURL: String): MediaSource {
