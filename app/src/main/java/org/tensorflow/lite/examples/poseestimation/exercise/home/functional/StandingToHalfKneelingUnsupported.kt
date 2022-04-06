0a6279e8578000096b03580328d6d1df91d99e79
Md. Rifaet Ullah Rifat <rifaet.ullah@mymedicalhub.com>
2022-04-06 13:59:05 +0600

    (DEV) Added 8 more exercises

---
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/exercise/home/functional/StandingToHalfKneelingUnsupported.kt
@@ -0,0 +1,7 @@
+package org.tensorflow.lite.examples.poseestimation.exercise.home.functional
+
+import android.content.Context
+import org.tensorflow.lite.examples.poseestimation.exercise.home.HomeExercise
+
+class StandingToHalfKneelingUnsupported(context: Context) :
+    HomeExercise(context = context, id = 661)
